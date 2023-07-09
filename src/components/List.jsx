ccb97e3fbc9b0c34cf4ad7051c753ef41ee4441e
jssolar <juansolarsolar@yahoo.com>
2023-07-08 23:53:21 -0400

    2 files modified, 1 added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -19,35 +19,33 @@ const List = () => {
     }
   };
   return (
-    <div className="input-group mb-3">
-      <div className="input-group-text">
-        <form onSubmit={handleSubmit}>
-          <input
-            className="input mt-0"
-            type="text"
-            value={tarea}
-            onChange={handleChange}
-          />
-          <button type="submit" onClick={handleClick}>
-            {nuevaTarea.length}
-          </button>
-          <ul>
-            {nuevaTarea.length > 0
-              ? nuevaTarea.map((item, index) => {
-                  return (
-                    <li
-                      className="list-group-item"
-                      onClick={handleClick}
-                      key={index}
-                    >
-                      {item}
-                    </li>
-                  );
-                })
-              : 'Ingresar nueva tarea'}
-          </ul>
-        </form>
-      </div>
+    <div className="input-group-text">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input mt-0"
+          type="text"
+          value={tarea}
+          onChange={handleChange}
+        />
+        <button className="rounded" type="submit" onClick={handleClick}>
+          {nuevaTarea.length}
+        </button>
+        <ul>
+          {nuevaTarea.length > 0
+            ? nuevaTarea.map((item, index) => {
+                return (
+                  <li
+                    className="list-group-item"
+                    onClick={handleClick}
+                    key={index}
+                  >
+                    {item}
+                  </li>
+                );
+              })
+            : 'Ingresar nueva tarea'}
+        </ul>
+      </form>
     </div>
   );
 };
